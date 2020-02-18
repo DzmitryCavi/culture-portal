3c0a05492787b1a0789b87dad6fdb88049649f24
Dzmitry Malakhouski <dzmitry.malakhouski@tut.by>
2020-02-18 14:12:10 +0300

    feat: project's temlate

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import './App.sass';
+
+import About from './containers/about/About';
+import Home from './containers/home/Home';
+
+function App() {
+
+  let arr = [2, 3]
+
+  let b = arr[0]
+
+  console.log("sfsdf")
+
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/about/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.sass';
+
+function About() {
+  return (
+    <div className="about">
+      About
+    </div>
+  );
+}
+
+export default About;
