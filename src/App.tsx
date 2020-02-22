7a8b16da96703cf16c78355a904e1a38de624163
bacila1990 <1smeh.ru@mail.ru>
2020-02-22 21:47:39 +0300

    feat: add page styleguide

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import './App.sass';
 
 import About from './containers/about/About';
 import Home from './containers/home/Home';
+import Styleguide from './containers/styleguide/Styleguide';
 
 function App() {
 
@@ -31,12 +32,18 @@ function App() {
               <li>
                 <Link to="/about">About</Link>
               </li>
+              <li>
+                <Link to="/styleguide">Styleguide</Link>
+              </li>
             </ul>
           </nav>
 
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
+            <Route path="/styleguide">
+              <Styleguide />
+            </Route>
             <Route path="/about">
               <About />
             </Route>
