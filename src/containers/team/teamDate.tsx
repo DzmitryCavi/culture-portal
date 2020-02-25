4c3bcf9ebe8029e2b8f16babcabe37ade83c3039
Yuri-9 <vashkevichyp@gmail.com>
2020-02-25 04:24:32 +0300

    feat: add page team

---
+++ src/containers/team/teamDate.tsx
@@ -0,0 +1,62 @@
+export const teamData = [
+  {
+    id: 0,
+    name: 'Dzmitry Malakhouski',
+    photoUrl: 'https://avatars2.githubusercontent.com/u/48457759?s=460&v=4',
+    linkGit: 'https://github.com/DzmitryCavi',
+    linkLinkedIn: 'https://www.linkedin.com/',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusamus corporis nemo expedita neque aspernatur adipisci
+        pariatur! Consequuntur perferendis veritatis possimus odit
+        laboriosam accusantium, nobis, ut eos officia mollitia unde
+        quidem.`,
+  },
+  {
+    id: 1,
+    name: 'Vasiliy Pisarenka',
+    photoUrl: 'https://avatars1.githubusercontent.com/u/25791669?s=400&v=4',
+    linkGit: 'https://github.com/bacila1990',
+    linkLinkedIn: 'https://www.linkedin.com/in/',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusamus corporis nemo expedita neque aspernatur adipisci
+        pariatur! Consequuntur perferendis veritatis possimus odit
+        laboriosam accusantium, nobis, ut eos officia mollitia unde
+        quidem.`,
+  },
+  {
+    id: 2,
+    name: 'Yura Vaskevich',
+    photoUrl: 'https://avatars2.githubusercontent.com/u/54293167?s=460&v=4',
+    linkGit: 'https://github.com/Yuri-9',
+    linkLinkedIn: 'https://www.linkedin.com/in/yuri-vashkevich-3561b11a2/',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusamus corporis nemo expedita neque aspernatur adipisci
+        pariatur! Consequuntur perferendis veritatis possimus odit
+        laboriosam accusantium, nobis, ut eos officia mollitia unde
+        quidem.`,
+  },
+  {
+    id: 3,
+    name: 'Yury Zikeyeu',
+    photoUrl: 'https://avatars0.githubusercontent.com/u/55135198?s=460&v=4',
+    linkGit: 'https://github.com/ziknation/',
+    linkLinkedIn: 'https://www.linkedin.com/in/',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusamus corporis nemo expedita neque aspernatur adipisci
+        pariatur! Consequuntur perferendis veritatis possimus odit
+        laboriosam accusantium, nobis, ut eos officia mollitia unde
+        quidem.`,
+  },
+  {
+    id: 4,
+    name: 'Aldar Antonov',
+    photoUrl: 'https://avatars3.githubusercontent.com/u/3716701?s=460&v=4',
+    linkGit: 'https://github.com/hardlin3r',
+    linkLinkedIn: 'https://www.linkedin.com/in/',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusamus corporis nemo expedita neque aspernatur adipisci
+        pariatur! Consequuntur perferendis veritatis possimus odit
+        laboriosam accusantium, nobis, ut eos officia mollitia unde
+        quidem.`,
+  },
+];
