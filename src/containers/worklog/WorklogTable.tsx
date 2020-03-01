9243c5f6a93a22ed14bac91a09a300b71ff851f6
Yuri Zikeev <zikeev.yura@gmail.com>
2020-03-01 17:13:58 +0300

    feat: add button group of team members worklog

--- src/containers/worklog/WorklogTable.tsx
+++ src/containers/worklog/WorklogTable.tsx
@@ -1,66 +1,53 @@
 import React from 'react';
-import { Typography, Box, Paper } from '@material-ui/core'
-import {TableContainer, Table, TableHead, TableRow, TableCell, TableBody} from '@material-ui/core'
+import { Typography, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import { Trans }  from 'react-i18next';
 import { worklogTables } from './worklogTables';
+import { ButtonGroup, Button } from '@material-ui/core';
+import TableComponent from './Table'
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 300,
-    maxWidth: 700,
-    margin: '0 auto'
-  },
-  typography: {
-    margin: 30,
-  },
+  buttons: {
+    maxWidth: 500,
+  }
 });
 
-function WorklogTable() {
-  const classes = useStyles();
+class WorklogTable extends React.Component {
+  state = {
+    TableCallState: false,
+    id: 0
+  }
 
-  return (
-    <Box>
-      <Box my={5}>
-        <Typography align='center' variant='h4' >
-          <Trans>
-            in:Worklog
-          </Trans>
-        </Typography>
-      </Box>
-      {worklogTables.map(table => (
-        <Box key={table.id}>
-          <Typography align='center' className={classes.typography} variant='h5' ><Trans>te:{table.id.toString()}.name</Trans></Typography>
-          <TableContainer className={classes.table} component={Paper}>
-            <Table aria-label='simple table'>
-              <TableHead>
-                <TableRow>
-                  <TableCell>
-                    <Trans>
-                      in:Time spent
-                    </Trans>
-                  </TableCell>
-                  <TableCell align='right'>
-                    <Trans>
-                      in:Performed work
-                    </Trans>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {table.performedWork.map(row => (
-                  <TableRow key={row.performedWork}>
-                    <TableCell>{row.timeSpent}</TableCell>
-                    <TableCell align='right'><Trans>te:{table.id.toString()}.performedWork.{row.performedWork}</Trans></TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+  handleClick = (event:any) => {
+    this.setState({
+      TableCallState: true,
+      id: event.currentTarget.value
+    })
+  }
+
+  render() {
+    return (
+      <Box>
+        <Box my={5}>
+          <Typography align='center' variant='h4' >
+            <Trans>
+              in:Worklog
+            </Trans>
+          </Typography>
         </Box>
-      ))}
-    </Box>
-  );
+        <Box>
+          <Box className='button_group_parent'>
+            <ButtonGroup className='button_group' variant='contained' color="primary" aria-label='contained primary button group'>
+              {worklogTables.map(table => (
+                <Button key={table.id.toString()} onClick={this.handleClick} value={table.id}><Trans>te:{table.id.toString()}.name</Trans></Button>
+              ))}
+            </ButtonGroup>
+          </Box>
+          {this.state.TableCallState ? <TableComponent memberId={this.state.id}/> : null}
+        </Box>
+      </Box>
+    )
+  }
 }
 
 export default WorklogTable
