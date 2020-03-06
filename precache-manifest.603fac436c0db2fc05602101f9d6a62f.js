1659df07480531a8b16c6d78ed67204dccb68c01
DzmitryCavi <dzmitry.malakhouski@tut.by>
2020-03-06 22:14:17 +0200

    deploy

---
+++ precache-manifest.603fac436c0db2fc05602101f9d6a62f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a86aa5fbd65eb26ae0878852709ff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9bd1ac93a8c6bea3ded",
+    "url": "/static/css/2.1dbf363f.chunk.css"
+  },
+  {
+    "revision": "ab9ec52b1a80bec41431",
+    "url": "/static/css/main.7f1c66ec.chunk.css"
+  },
+  {
+    "revision": "d9bd1ac93a8c6bea3ded",
+    "url": "/static/js/2.2f6a1c73.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/static/js/2.2f6a1c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9ec52b1a80bec41431",
+    "url": "/static/js/main.87cac983.chunk.js"
+  },
+  {
+    "revision": "36e0055b1b92471b4352",
+    "url": "/static/js/runtime-main.0367b94b.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
